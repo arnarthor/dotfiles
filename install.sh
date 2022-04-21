2c8a85c85b3e3b7e07d241da6cc0426d24c2d789
arnarthor <arnar.th92@gmail.com>
2022-04-21 00:55:00 +0000

    Check if .bashrc file exists

--- install.sh
+++ install.sh
@@ -16,5 +16,5 @@ fi
 
 # Symlink configs
 [ ! -L "$HOME/.zshrc" ] && ln -s ./zshrc ~/.zshrc
-[ -L "$HOME/.bashrc" ] && rm -rf ~/.bashrc
+[ -f "$HOME/.bashrc" ] && rm -rf ~/.bashrc
 ln -s ./bashrc ~/.bashrc
